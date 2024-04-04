7a6d79b0c379c2c24fd9198bcba0a961f854c0b3
JoshRod2483 <JoshRod2483@gmail.com>
2024-04-04 14:58:52 -0500

    Update

--- src/CreateController.py
+++ src/CreateController.py
@@ -1,2 +1,14 @@
 #press alt + Shift + m
 import maya.cmds as mc
+
+from PySide2.QtWidgets import QWidget
+
+class CreateControllerWidget(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.setWindowTitle("Create IK/FK limb")
+
+
+controllerWidget = CreateControllerWidget()
+controllerWidget.show()
+
